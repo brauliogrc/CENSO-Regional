a8dfb0c22e769cc9d96c6d2a41c1d1941b654c34
uib44927 <uib44927@contiwan.com>
2021-06-22 12:22:43 -0500

    Version 3.5.2

---
+++ CensoAPI02/CensoAPI02/Migrations/20210622171138_Version3.5.1.2AddFieldEmployeeNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CensoAPI02.Migrations
+{
+    public partial class Version3512AddFieldEmployeeNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "uEmployeeNumber",
+                table: "HRU",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "uEmployeeNumber",
+                table: "HRU",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
