8e71c31b304f49f59695dd5b49fcdd657c10cc90
uib97447 <gl_localit04.intern-ext@functionalaccount.com>
2021-06-24 02:02:46 -0500

    Version 3.5.3

---
+++ Censo002/src/ventanas.js
@@ -0,0 +1,11 @@
+function mostrarRegistro(){
+    document.getElementById('registro').style.display='block';
+    document.getElementById('consultarf').style.display='block';
+    document.getElementById('registro01').style.display='block';
+    document.getElementById('buscar').style.display='none';
+  }
+  function mostrarbuscar(){
+    document.getElementById('registro01').style.display='none';
+    document.getElementById('buscar').style.display='block';
+    document.getElementById('consultarf').style.display='none';
+  }
