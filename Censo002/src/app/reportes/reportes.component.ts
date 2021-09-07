398fe9f859cdc6a75d69f7f105ce43553031b2e2
brauliogrc <brauliogrc95@mail.com>
2021-09-07 13:43:33 -0500

    Version0.0.6

--- Censo002/src/app/reportes/reportes.component.ts
+++ Censo002/src/app/reportes/reportes.component.ts
@@ -5,6 +5,12 @@ import { ConverToObjectArray } from 'src/assets/ts/exportDataFormat';
 import { ExportData } from '../../assets/ts/interfaces/exportData';
 import { ShowErrorService } from '../services/newServices/ShowErrors/show-error.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import {
+  Theme,
+  ticketList,
+} from '../../../productivo/assets/ts/interfaces/newInterfaces';
+import { FieldsService } from '../services/newServices/Fields/fields.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reportes',
@@ -12,8 +18,13 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./reportes.component.css'],
 })
 export class ReportesComponent implements OnInit {
+  Theme: Theme[] = [];
+  private locationId: number = Number(sessionStorage.getItem('location'));
+
   constructor(
     private router: Router,
+    private _fb: FormBuilder,
+    private _fields: FieldsService,
     private _showError: ShowErrorService,
     private _createReport: CreateReportService
   ) {}
@@ -36,11 +47,26 @@ export class ReportesComponent implements OnInit {
       return;
     }
     this.getReportData();
+    this.getTheme();
+  }
+
+  getTheme(): void {
+    this.Theme = [];
+    this._fields.getThme(this.locationId).subscribe(
+      (data) => {
+        this.Theme = [...data];
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.error.message);
+        this._showError.statusCode(error);
+      }
+    );
   }
 
   private objectArray = new ConverToObjectArray();
 
   ticketList: ExportData[];
+  private back: ExportData[];
 
   getReportData(): void {
     this._createReport
@@ -70,10 +96,57 @@ export class ReportesComponent implements OnInit {
 
   getList() {
     this.ticketList = this.objectArray.ticketList;
+    this.back = this.objectArray.ticketList;
     console.log(this.objectArray.ticketList);
   }
 
   exportToExcel() {
     this._createReport.exportToExcel(this.ticketList, 'TicketsCENSO');
+  }
+
+  // Filtrado de los tickets
+  filterBy = this._fb.group({
+    from: [''],
+    to: [''],
+    theme: [''],
+  });
+
+  filter(): void {
+    this.ticketList = [...this.back];
+
+    console.log(this.filterBy.value.theme);
+    
+
+    if (this.filterBy.value.from != '') {
+      this.ticketList = this.ticketList.filter(
+        (newList) => newList.creationDate >= new Date(this.filterBy.value.from)
+      );
+    }
+    if (this.filterBy.value.to != '') {
+      this.ticketList = this.ticketList.filter(
+        (newList) => newList.creationDate <= new Date(this.filterBy.value.to)
+      );
+    }
+    if (this.filterBy.value.theme != '') {
+      this.ticketList = this.ticketList.filter(
+        (newList) => newList.theme === this.filterBy.value.theme
+      );
+    }
+
+    console.log(this.ticketList);
+  }
+
+  today(): string {
+    let day: number | string = new Date().getDate();
+    let month: number | string = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+
+    day < 10 ? (day = '0' + day) : day;
+    month < 10 ? (month = '0' + month) : month;
+
+    const fullDate: string = year + '-' + month + '-' + day;
+    console.log(fullDate);
+
+    return fullDate;
   }
 }
